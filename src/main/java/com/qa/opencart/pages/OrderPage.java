2acdb5b4469e9f00a154cbaa8032f8b8c5b5f2c4
Lijesh E M <lijesh.m@247-inc.com>
2022-10-17 16:00:03 +0530

    adding by locators in orderpage

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -1,8 +1,11 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class OrderPage {
 	
 	String order;
+	private By orderid = By.id("orderid");
 	public void getOrder() {
 		System.out.println("order id is 123...");
 	}
